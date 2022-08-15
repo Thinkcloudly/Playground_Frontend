f0de3c07e5802c4789e549416ba4da1b1e977da1
PrashantGuptaa <meengineerprashant@gmail.com>
2022-08-15 17:59:25 +0530

    Removed unused code

--- src/containers/Instructions/instructions.jsx
+++ src/containers/Instructions/instructions.jsx
@@ -2,22 +2,19 @@ import React, { useEffect, useState } from 'react'
 import Markdown from 'markdown-to-jsx';
 import './instructions.css';
 import { useParams, useSearchParams } from 'react-router-dom';
-import AwsAmplifyCongnitoAuth from '../../utils/AwsAmplifyCognitoAuth';
 import httpServices from './../../services/http.service';
 import { deleteEnvironmentEndpoint, validateScenerioEndpoint } from '../../configs/apiEndpoints';
 import { get } from 'lodash';
-import { failureInValidation, successfullyValidatedscenario, snackBarAlertLevels, ENV_STACK_ID } from '../../configs/constants';
+import { failureInValidation, successfullyValidatedscenario, snackBarAlertLevels } from '../../configs/constants';
 import SnackBar from '../../components/SnackBar';
 import { deletedEnvironmentSuccessfully } from './../../configs/constants';
 import { CircularProgress } from '@mui/material';
-import Cookies from 'js-cookie';
 
 const { success, warning, error } = snackBarAlertLevels;
 
 const Instructions = () => {
 
   const [instructions, setInstructions] = useState('');
-  const [userName, setUserName] = useState('');
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState('');
   const [alertLevel, setAlertLevel] = useState('');
@@ -30,20 +27,7 @@ const Instructions = () => {
 
   useEffect(() => {
     fetchMarkDownFileContent();
-    getUserName();
   }, []);
-
-  const getUserName = async () => {
-    if (Cookies.get('userName')) {
-      setUserName(Cookies.get('userName'));     
-      return;
-    }
-    const amplifyAuth = new AwsAmplifyCongnitoAuth();
-    const user = await amplifyAuth.getUserNameFromAmplify();
-    setUserName(user);
-  }
-
-
 
   const fetchMarkDownFileContent = async () => {
     try {
