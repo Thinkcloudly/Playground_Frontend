cb6a89d4271db3a4a3899e2032f94238be993f32
PrashantGuptaa <meengineerprashant@gmail.com>
2022-09-19 22:01:41 +0530

    Removed unused code

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import {Modal, CircularProgress } from '@mui/material';
 import { customFlex } from './../../globalStyles/flexStyling';
 
